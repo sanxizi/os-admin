072b680761756decae837b64100be2ef4d9b795f
salmon <3307361757@qq.com>
2018-12-05 13:12:17 +0800

    firstView

---
+++ application/os/controller/Index.php
@@ -0,0 +1,56 @@
+<?php
+namespace app\os\controller;
+use think\Controller;
+
+class Index extends Controller
+
+{
+    public function index()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function user()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function message()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function about()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function cog()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function picture()
+    {
+
+        return $this -> fetch();
+
+    }
+
+    public function login()
+    {
+
+        return $this -> fetch();
+
+    }
+}
